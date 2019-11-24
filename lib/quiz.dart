1b40d029dfcf2e24c05beb3cb480051ba56792c4
Markus Sterner <markus.sterner88@gmail.com>
2019-11-24 10:34:53 +0100

    three steps with solution

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,13 +1,15 @@
 import 'dart:math';
 
 import 'package:color_quiz/selected_color_painter.dart';
-import 'package:color_quiz/utils/custom_color_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class Quiz extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => QuizState();
 }
+
+enum Stage { SHOW_TARGET_COLOR, SHOW_COLOR_PICKER, SHOW_SOLUTION }
 
 class QuizState extends State<Quiz> {
   int _targetR;
@@ -21,47 +23,25 @@ class QuizState extends State<Quiz> {
 
   int _points = 0;
 
+  Stage _stage = Stage.SHOW_TARGET_COLOR;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _targetColor,
+      backgroundColor: Color.fromRGBO(15, 15, 15, 1.0),
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            CustomColorPicker(
-                onColorChanged: (Color selectedColor) => this.setState(() {
-                      this._selectedColor = selectedColor;
-                    })),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Center(
-                    child: CustomPaint(
-                      painter: SelectedColorPainter(this._selectedColor),
-                    ),
-                  ),
-                ),
-              ],
+            Text(
+              '$_points Points',
+              style: TextStyle(color: Colors.white),
             ),
-            Column(
-              children: <Widget>[
-                Text(
-                  '$_points Points',
-                  style: TextStyle(color: _displayColor),
-                ),
-                Expanded(
-                  child: Center(
-                    child: RaisedButton(
-                      child: Text(
-                        'Make a guess',
-                        style: TextStyle(color: _displayColor),
-                      ),
-                      onPressed: _onGuessPressed,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            if (_stage == Stage.SHOW_TARGET_COLOR)
+              ..._getShowTargetColorStateWidgets()
+            else if (_stage == Stage.SHOW_COLOR_PICKER)
+              ..._getShowColorPickerStateWidgets()
+            else if (_stage == Stage.SHOW_SOLUTION)
+              ..._getShowSoultionStateWidgets()
           ],
         ),
       ),
@@ -73,11 +53,99 @@ class QuizState extends State<Quiz> {
     _setNewColors();
   }
 
-  _onGuessPressed() {
-    _setNewColors();
+  List<Widget> _getShowTargetColorStateWidgets() {
+    return [
+      Expanded(
+        child: new Container(
+          color: _targetColor,
+        ),
+      ),
+      Center(
+        child: RaisedButton(
+          child: Text(
+            'Make a guess',
+          ),
+          onPressed: _onMakeGuessPressed,
+        ),
+      ),
+    ];
   }
 
-  _setNewColors() {
+  List<Widget> _getShowColorPickerStateWidgets() {
+    return [
+      Expanded(
+        child: Center(
+          child: CustomPaint(
+            painter: SelectedColorPainter(_selectedColor),
+          ),
+        ),
+      ),
+      ColorPicker(
+        pickerColor: _selectedColor,
+        onColorChanged: (Color selectedColor) {
+          setState(() {
+            _selectedColor = selectedColor;
+            print(selectedColor);
+          });
+        },
+        enableLabel: true,
+        pickerAreaHeightPercent: 0.8,
+      ),
+      RaisedButton(
+        child: Text(
+          'Submit',
+        ),
+        onPressed: _onSubmitPressed,
+      ),
+    ];
+  }
+
+  List<Widget> _getShowSoultionStateWidgets() {
+    return [
+      Expanded(
+        child: Container(
+          color: _targetColor,
+          child: Center(
+            child: Text(
+              'Target Color',
+              style: TextStyle(color: _displayColor),
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        child: Container(
+          color: _selectedColor,
+          child: Center(
+            child: Text(
+              'Your Guess',
+              style: TextStyle(color: _displayColor),
+            ),
+          ),
+        ),
+      ),
+      RaisedButton(
+        child: Text(
+          'Continue',
+        ),
+        onPressed: _onContinuePressed,
+      ),
+    ];
+  }
+
+  void _onMakeGuessPressed() {
+    setState(() {
+      _stage = Stage.SHOW_COLOR_PICKER;
+    });
+  }
+
+  void _onSubmitPressed() {
+    setState(() {
+      _stage = Stage.SHOW_SOLUTION;
+    });
+  }
+
+  void _setNewColors() {
     setState(() {
       _targetR = new Random().nextInt(255);
       _targetG = new Random().nextInt(255);
@@ -89,6 +157,13 @@ class QuizState extends State<Quiz> {
           : Color.fromRGBO(255, 255, 255, 1.0);
 
       print('$_targetR, $_targetG, $_targetB');
+    });
+  }
+
+  void _onContinuePressed() {
+    _setNewColors();
+    setState(() {
+      _stage = Stage.SHOW_TARGET_COLOR;
     });
   }
 }
--- lib/selected_color_painter.dart
+++ lib/selected_color_painter.dart
@@ -11,7 +11,7 @@ class SelectedColorPainter extends CustomPainter {
     // set the color property of the paint
     paint.color = this.color;
     // center of the canvas is (x,y) => (width/2, height/2)
-    var center = Offset(size.width / 2, size.height / 2);
+    var center = Offset(0, 0);
 
     // draw the circle on centre of canvas having radius 75.0
     canvas.drawCircle(center, 75.0, paint);
