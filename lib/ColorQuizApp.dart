9728cf0fccc27f79f01c385f824eccb1088a01f9
Markus Sterner <markus.sterner88@gmail.com>
2020-03-15 20:39:28 +0100

    Dark mode, app header in Settings

--- lib/ColorQuizApp.dart
+++ lib/ColorQuizApp.dart
@@ -1,15 +1,27 @@
+import 'package:color_quiz/theme.dart';
 import 'package:color_quiz/widgets/highscore/HighScore.dart';
 import 'package:color_quiz/widgets/home/Home.dart';
+import 'package:color_quiz/widgets/settings/ColorOverview.dart';
 import 'package:color_quiz/widgets/settings/Settings.dart';
 import 'package:flutter/material.dart';
 
 class ColorQuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: '/', routes: {
-      '/': (context) => Home(),
-      '/highscore': (context) => HighScore(),
-      '/settings': (context) => Settings(),
-    });
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/highscore': (context) => HighScore(),
+        '/settings': (context) => Settings(),
+        '/settings/color-overview': (context) => ColorOverview(),
+      },
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: COLOR_BLACK,
+        focusColor: COLOR_WHITE,
+        hoverColor: COLOR_WHITE,
+      ),
+    );
   }
 }
--- lib/widgets/settings/Settings.dart
+++ lib/widgets/settings/Settings.dart
@@ -1,30 +1,25 @@
-import 'package:color_quiz/theme.dart';
-import 'package:color_quiz/widgets/common/DefaultButton.dart';
-import 'package:color_quiz/widgets/settings/ColorOverview.dart';
 import 'package:flutter/material.dart';
+
+import '../../theme.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Text('Settings', style: TextStyle(color: COLOR_WHITE, fontSize: 40)),
-          DefaultButton(
-            label: 'Color Overview',
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return ColorOverview();
-                  },
-                ),
-              );
-            },
-            color: COLOR_WHITE,
-          ),
-        ],
-      ),
-    );
+    return Scaffold(
+        backgroundColor: COLOR_BLACK,
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                title: Text('Color Overview'),
+                onTap: () =>
+                    Navigator.pushNamed(context, '/settings/color-overview'),
+              ),
+            ),
+          ],
+        ));
   }
 }
