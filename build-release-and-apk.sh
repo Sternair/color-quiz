3c762080aa7fdc0966b4c38a2fdb47192ea14049
Markus Sterner <markus.sterner88@gmail.com>
2020-03-27 18:40:22 +0100

    ios deployment

--- build-release-and-apk.sh
+++ build-release-and-apk.sh
@@ -4,4 +4,4 @@ rm ./app-release.apks
 
 bundletool build-apks --bundle=./build/app/outputs/bundle/release/app-release.aab --output=./app-release.apks
 
-# bundletool install-apks --apks=./app-release.apks --device-id=WCR0218828001728
+bundletool install-apks --apks=./app-release.apks --device-id=WCR0218828001728
